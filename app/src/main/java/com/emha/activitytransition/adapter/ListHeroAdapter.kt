39e29b12bc66e8a74227934d52d218ba9fc656b0
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-09-19 08:50:36 +0700

    first commit

---
+++ app/src/main/java/com/emha/activitytransition/adapter/ListHeroAdapter.kt
@@ -0,0 +1,67 @@
+package com.emha.activitytransition.adapter
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.emha.activitytransition.HeroActivity
+import com.emha.activitytransition.R
+import com.emha.activitytransition.model.Hero
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
+    RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listHero[position])
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var imgPhoto: ImageView = itemView.findViewById(R.id.profileImageView)
+        private var tvName: TextView = itemView.findViewById(R.id.nameTextView)
+        private var tvDescription: TextView = itemView.findViewById(R.id.descTextView)
+
+        fun bind(hero: Hero) {
+            Glide.with(itemView.context)
+                .load(hero.photo)
+                .circleCrop()
+                .into(imgPhoto)
+            tvName.text = hero.name
+            tvDescription.text = hero.description
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, HeroActivity::class.java)
+                intent.putExtra("Hero", hero)
+//                itemView.context.startActivity(
+//                    intent,
+//                    ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity)
+//                        .toBundle()
+//                )
+
+                val optionsCompat: ActivityOptionsCompat =
+                    ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        itemView.context as Activity,
+                        Pair(imgPhoto, "profile"),
+                        Pair(tvName, "name"),
+                        Pair(tvDescription, "description"),
+                    )
+                itemView.context.startActivity(intent, optionsCompat.toBundle())
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/emha/activitytransition/model/Hero.kt
@@ -0,0 +1,11 @@
+package com.emha.activitytransition.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
